6feacd7fc41a137ad0a860d98351a6e5b86a42e2
RaquelSanntos <santos.soares0424@gmail.com>
2022-09-09 11:06:30 -0400

    atualizando as classes

---
+++ src/Conta.java
@@ -0,0 +1,34 @@
+
+
+public class Conta {
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+	String cpf;
+	String profissao;
+	
+	void deposita(double valor) {
+		this.saldo+= valor;
+
+	}
+	public boolean saca (double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			System.out.println("valor sacado");
+			return true;
+		}else {
+			System.out.println("valor insuficiente");
+			return false;
+			
+		}
+	}
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo>= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
+}
