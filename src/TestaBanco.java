df73ef5bed4fd5687c607796d8b6d368d31bc34f
RaquelSanntos <santos.soares0424@gmail.com>
2022-09-10 22:20:45 -0400

    Identacão + Alteração

--- src/TestaBanco.java
+++ src/TestaBanco.java
@@ -2,12 +2,15 @@
 public class TestaBanco {
 	public static void main(String[] args) {
 		Cliente paulo = new Cliente();
-		paulo.nome	= "Paulo Silveira";
+		paulo.nome	= "Raquel Santos";
 		paulo.cpf	= "222.222.222-22";
 		paulo.profissao = "programador";
 		
-		Conta contaDConta	= new Conta();
-		contaDConta.deposita(100);
+		Conta contaDoPaulo	= new Conta();
+		contaDoPaulo.deposita(100);
+		contaDoPaulo.titular = paulo;
+		System.out.println(contaDoPaulo.titular);
+		System.out.println(contaDoPaulo.titular.nome);
 	}
 
 }
